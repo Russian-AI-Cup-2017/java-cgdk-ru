e682faafd6b2ba4d15662b310971d49c0fdf445d
SladeThe <sladethe@gmail.com>
2017-11-15 20:05:25 +0400

    Tiny changes.

--- src/main/java/model/FacilityType.java
+++ src/main/java/model/FacilityType.java
@@ -7,6 +7,7 @@ public enum FacilityType {
     /**
      * Центр управления. Увеличивает возможное количество действий игрока на
      * {@code game.additionalActionCountPerControlCenter} за {@code game.actionDetectionInterval} игровых тиков.
+     * Также немного уменьшает задержку между двумя последовательными тактическими ядерными ударами.
      */
     CONTROL_CENTER,
 
